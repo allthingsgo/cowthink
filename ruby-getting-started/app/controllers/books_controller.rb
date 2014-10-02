8d78bb2788dfb43b2d5bdfe53e9f746abf1e18b9
S Ye <badvogato@gmail.com>
2014-10-01 23:06:53 -0400

    add Proj/ruby-getting-started

---
+++ ruby-getting-started/app/controllers/books_controller.rb
@@ -0,0 +1,6 @@
+class BooksController < ApplicationController
+  # GET /books
+  def index
+  #  @books = Book.all
+  end
+end
---
+++ ruby-getting-started/bundle/ruby/2.0.0/gems/pg-0.17.1/ext/siteconf20140926-9840-w1jzcu.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140926-9840-1umcbsj"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
